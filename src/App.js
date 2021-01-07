2f3b79aaf9ddd07995a43db75a0babaebe57016a
rubysattar <rubysattar@gmail.com>
2021-01-07 15:31:01 -0500

    added list items component and initial styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ListItems from './ListItems'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this)
+    this.addItem = this.addItem.bind(this)
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault()
+    const newItem = this.state.currentItem
+    if(newItem.text !== ''){
+      const newItems=[...this.state.items, newItems]
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter Text" value={this.state.currentItem.text} onChange={this.handleInput}/>
+
+            <button type="submit">Add task</button>
+          </form>
+        </header>
+        <ListItems items = {this.state.items}></ListItems>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/ListItems.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ListItems.css'
+
+function ListItems(props){
+    const items = props.items
+    const listItems = items.map(item => {
+        return <div className="list" key="item.key">
+            <p>{item.text}</p>
+        </div>
+    })
+    return(
+
+    )
+}
+
+export default ListItems
